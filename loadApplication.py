ed0d7a68fb494198351b1e6a4bb77ce4c21904d5
Administrator <Administrator@WIN-J3EQULD7DG7>
2019-05-28 18:42:32 +0800

    20190528

--- loadApplication.py
+++ loadApplication.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-import sys,os,time,shutil
-
+import sys,os,time,shutil,json
+import datetime
+import urllib3
+import urllib3.contrib.pyopenssl
+import certifi
 from scrapy.selector import Selector 
 
 import tornado.ioloop
@@ -11,6 +14,56 @@ from app.init.populateWebComponent import WebApplication
 from app.init.fileBasedConfiguration import ApplicationProperties
 from app.context.initializedInstancePool import ApplicationContext 
 
+async def async_fetch_https(url):
+    header = {
+        #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) VAR/1.0.0.1"
+    }
+    http_client = httpclient.AsyncHTTPClient()
+    try:
+        response = await http_client.fetch(url,method='GET',headers=header)
+        return response.body
+    except Exception as e:
+        print("Error: %s" % e)
+        return ''
+
+def fetch_https(url):
+    #if scenarioId == 'sid-003' or scenarioId == 'sid-007' or scenarioId == 'sid-0013' or scenarioId == 'sid-0016':
+     #   await gen.sleep(15)
+    
+    print('[%s] fetch_https |  url[%s]'%(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), url))    
+    header = {
+        #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) VAR/1.0.0.1"
+    }
+    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    response = http.request('GET', url, None, header)
+    return response.data
+        
+def corpus(pageno, pages, content, parent=None):
+    page = pages[pageno]
+    selector = page['selector']
+    extract = page['extract'] if "extract" in page else 'False'
+    wholeTxt =  page['wholeTxt'] if "wholeTxt" in page else 'False'
+    
+    sel = Selector(text=content,type='html')
+    items = sel.xpath(selector['xpath'])
+    for item in items:
+        itemObj={}
+        itemObj['children']=[]
+        if extract == 'True':
+            href = item.xpath('@href').get()
+            itemObj['href'] = href
+            #body = async_fetch_https(href)
+            body = fetch_https(href)
+            corpus(pageno+1, pages, content=body, parent=itemObj)
+        if wholeTxt == 'True':
+            itemObj['text'] = item.xpath('//*/text()').getall()
+        else:
+            itemObj['text'] = item.xpath('text()').get()
+        parent['children'].append(itemObj)
+    return
+    
 def parse(content):
     print(content)
     selector = Selector(text=content,type='html')
@@ -41,10 +94,18 @@ async def worker(idx, parent):
                             props[pair[0]] = pair[1].strip('\n\r\t ')
                 
                 html = open(props['htmlPath'], 'r')
-                values = parse(content=html.read())
+                task = json.loads(props['task'])
+                #values = parse(content=html.read())
+                values={}
+                values['children']=[]
+                values['scenarioId']=task['scenarioId']
+                values['userId']=task['userId']
+                if task['sceType'] == 'corpus':
+                    corpus(pageno=0, pages=task['pages'], content=html.read(), parent=values)
                 
+                print(values)
                 res = open('F:\\var\\data\\result', 'ab+')
-                res.write(bytes(values,encoding='utf-8'))
+                res.write(bytes(json.dumps(values),encoding='utf-8'))
                 res.close()
                 #f.close()
                 #os.remove(parent+'/'+idx+'/'+file)
@@ -92,6 +153,7 @@ def Main(p_command=None):
     #ioLoop.run_sync()
   
 if __name__ == '__main__':
+    urllib3.contrib.pyopenssl.inject_into_urllib3()
     props={}
     if len(sys.argv) > 1:
         for idx in range(1, len(sys.argv)):
