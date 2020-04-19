cb0b59523bed6f679ebb3b87b5cfba5183d19f2f
liulin <liulin53@hotmail.com>
2020-04-19 23:13:09 +0800

    20200419

--- app/init/fileBasedConfiguration.py
+++ app/init/fileBasedConfiguration.py
@@ -1,20 +1,91 @@
 # -*- coding: utf-8 -*-
 import sys,os
 
+import logging
+from logging import handlers
+
+import psycopg2
+from DBUtils.PooledDB import PooledDB
+
+class Logger(object):
+    level_relations = {
+        'debug':logging.DEBUG,
+        'info':logging.INFO,
+        'warning':logging.WARNING,
+        'error':logging.ERROR,
+        'crit':logging.CRITICAL
+    }
+    logger=None
+    
+    def debug(self, msg):
+        self.logger.debug(msg)
+    def info(self, msg):
+        self.logger.info(msg)
+    def warning(self, msg):
+        self.logger.warning(msg)
+    def error(self, msg):
+        self.logger.error(msg)
+    def critical(self, msg):
+        self.logger.critical(msg)
+    
+    def __init__(self,filename,module=None, level='info',when='D',backCount=7,fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        if module:
+            format_str = logging.Formatter('%(asctime)s - thread[%(thread)d] -- ['+module+'] - %(levelname)s:    %(message)s')
+        else:
+            format_str = logging.Formatter(fmt)
+        self.logger.setLevel(self.level_relations.get(level))
+        sh = logging.StreamHandler()
+        sh.setFormatter(format_str) 
+        th = handlers.TimedRotatingFileHandler(filename=filename,when=when,backupCount=backCount,encoding='utf-8')
+        th.setFormatter(format_str)
+        self.logger.addHandler(sh) 
+        self.logger.addHandler(th)
+        
 class ApplicationProperties(object):
+    configures = {}
+    
+    @staticmethod
+    def populate(p_command=None):
+        if p_command and "config" in p_command:
+            project_path = p_command["config"]
+        else:
+            project_path = sys.path[0]
+        
+        if( os.path.exists( project_path + '/application.properties' ) ):
+            file = open(project_path + '/application.properties', 'r')
+            for line in file.readlines():
+                pair = line.split("=")
+                if len(pair) != 2:
+                    continue
+                ApplicationProperties.configures[pair[0].strip('\n\r\t ')] = pair[1].strip('\n\r\t ')
+        
+        if p_command:
+            for item in p_command.items():
+                ApplicationProperties.configures[item[0]] = item[1]
+     
+    @staticmethod
+    def configure(p_key):      
+        return ApplicationProperties.configures[p_key] if p_key in ApplicationProperties.configures else None
     
     def __init__(self, p_command=None):
         self.__properties = {}
-        self.__project_path = sys.path[0]
+        if p_command and "config" in p_command:
+            self.__project_path = p_command["config"]
+        else:
+            self.__project_path = sys.path[0]
         
         if( os.path.exists( self.__project_path + '/application.properties' ) ):
             file = open(self.__project_path + '/application.properties', 'r')
             for line in file.readlines():
                 pair = line.split("=")
-                self.__properties[pair[0]] = pair[1].strip('\n\r\t ')
+                if len(pair) != 2:
+                    continue
+                self.__properties[pair[0].strip('\n\r\t ')] = pair[1].strip('\n\r\t ')
         
-        for item in p_command.items():
-            self.__properties[item[0]] = item[1]
+        if p_command:
+            for item in p_command.items():
+                self.__properties[item[0]] = item[1]
             
     def getProperty(self, p_key):
         return self.__properties[p_key] if p_key in self.__properties else None
@@ -22,3 +93,68 @@ class ApplicationProperties(object):
     def list(self):
         for item in self.__properties.items():
             print( "%s=%s" % (item[0], item[1]) )
+            
+        
+class PgConnectionPool(object):
+    pool = None
+    
+    @staticmethod
+    def ini(host,port,user,password,database):
+        PgConnectionPool.pool = PooledDB(
+                                                    creator=psycopg2,  
+                                                    maxconnections=3,  
+                                                    mincached=1,  
+                                                    maxcached=2,  
+                                                    maxshared=2,  
+                                                    blocking=True,  
+                                                    maxusage=None,  
+                                                    setsession=[],  
+                                                    ping=0,
+                                                    host=host,
+                                                    port=port,
+                                                    user=user,
+                                                    password=password,
+                                                    database=database
+                                                    )
+    @staticmethod
+    def getConn():
+        conn = PgConnectionPool.pool.connection()
+        cursor = conn.cursor()
+        return conn,cursor
+    
+    @staticmethod
+    def release(conn, cur):
+        if conn:
+            conn.close()
+        if cur:
+            cur.close()
+            
+    def __init__(self, p_properties=None):
+        self._pool = PooledDB(
+                                                    creator=psycopg2,  
+                                                    maxconnections=3,  
+                                                    mincached=1,  
+                                                    maxcached=2,  
+                                                    maxshared=2,  
+                                                    blocking=True,  
+                                                    maxusage=None,  
+                                                    setsession=[],  
+                                                    ping=0,
+                                                    host=p_properties.getProperty("application.storage.postgres.connection.host"),
+                                                    port=p_properties.getProperty("application.storage.postgres.connection.port"),
+                                                    user=p_properties.getProperty("application.storage.postgres.connection.user"),
+                                                    password=p_properties.getProperty("application.storage.postgres.connection.password"),
+                                                    database=p_properties.getProperty("application.storage.postgres.connection.database")
+                                                    )
+
+    def getConnection(self):
+        conn = self._pool.connection()
+        cursor = conn.cursor()
+        return conn,cursor
+    
+    def close(self, conn, cur):
+        if conn:
+            conn.close()
+        if cur:
+            cur.close()
+            
--- loadApplication.py
+++ loadApplication.py
@@ -1,294 +1,88 @@
 # -*- coding: utf-8 -*-
-import sys,os,time,shutil,json,traceback
-import datetime
+import sys,os,time,shutil,json,traceback,requests,re
+from datetime import datetime
 import urllib3
 import urllib3.contrib.pyopenssl
 import certifi
-from scrapy.selector import Selector 
 
-import tornado.ioloop
-import tornado.web
-from tornado import gen, httpclient, ioloop, queues
+from scrapy.selector import Selector
 
-from app.init.populateWebComponent import WebApplication 
-from app.init.fileBasedConfiguration import ApplicationProperties
-from app.context.initializedInstancePool import ApplicationContext 
-from idlelib.iomenu import encoding
+from app.init.fileBasedConfiguration import ApplicationProperties,Logger
 
-inq = queues.Queue()
-fetch_header = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) VAR/1.0.0.1"
-    }
-async def async_fetch_https(url):
-    
-    http_client = httpclient.AsyncHTTPClient()
+from flask import Flask, request, make_response
+app = Flask(__name__)
+
+ApplicationProperties.populate()
+logger = Logger(module='LoadApplication', filename=ApplicationProperties.configure("application.logger.output.filename"))
+
+def response(msg='', status=200):      
+    response = make_response(msg, status)
+    response.headers['Content-Type'] = 'text/json; charset=utf-8'
+    return response      
+                                   
+@app.route('/pycloud/fetchPage', methods=['POST'])
+def calc():
     try:
-        response = await http_client.fetch(url,method='GET',headers=fetch_header)
-        return response.body
+        if request.method == "POST":
+             src=None
+             body = request.json
+             logger.info("fetch page: %s"%(body['url']))
+             url=body['url']
+             r = requests.get(url)
+             selector = Selector(text=r.text)
+             metaChr = selector.xpath('//meta/@charset')
+             charset = 'utf-8'
+             if  metaChr  :
+                charset = metaChr.get()                
+             else:
+                metaChr = selector.xpath("//meta[contains('http-equiv','Content-Type')]/@content")
+                if metaChr:
+                    charsetM=re.search(r'(?<=charset=)\w+',metaChr.get().lower())
+                    if charsetM:
+                        charset=charsetM.group(0)
+             logger.info('Page encoding charset is %s'%(charset))
+             
+             pattern = re.compile(r"<script.*>.*</script>|<script.*/>|<link.*[/]?>",re.I)
+             prehtm = pattern.sub('',r.text)
+             
+             seapre = re.finditer(r'<script.*>',prehtm)
+             seasuf = re.finditer(r'</script>',prehtm)
+             if seapre and seasuf:
+                filterIndexPair=[]  
+                for match in seapre:
+                    pair=[]
+                    s = match.start()
+                    e = match.end()
+                    pair.append(s)
+                    filterIndexPair.append(pair)
+                for idx, match in enumerate(seasuf):
+                    s = match.start()
+                    e = match.end()
+                    pair = filterIndexPair[idx]
+                    pair.append(e)
+                secondDomain = re.search(r"[A-Za-z0-9\-]+\.com|[A-Za-z0-9\-]+\.edu.cn|[A-Za-z0-9\-]+\.cn|[A-Za-z0-9\-]+\.com.cn|[A-Za-z0-9\-]+\.org|[A-Za-z0-9\-]+\.net|[A-Za-z0-9\-]+\.tv|[A-Za-z0-9\-]+\.vip|[A-Za-z0-9\-]+\.cc|[A-Za-z0-9\-]+\.gov.cn|[A-Za-z0-9\-]+\.gov|[A-Za-z0-9\-]+\.edu|[A-Za-z0-9\-]+\.biz|[A-Za-z0-9\-]+\.net.cn|[A-Za-z0-9\-]+\.org.cn",url.lower()).group()
+                path = ApplicationProperties.configure("application.storage.filesystem.download.staticdir")+'/'+secondDomain
+                if not os.path.exists(path):
+                    os.mkdir(path)
+                filename=datetime.now().strftime('%Y%m%d%H%M%S')+'.html'
+                fp = open(path+"/"+filename, 'w+', encoding=charset)
+                s=0
+                for item in filterIndexPair:
+                    e=item[0]-1
+                    fp.write(prehtm[s:e])
+                    s=item[1]+1
+                fp.write(prehtm[s:])
+                fp.close()
+                src = secondDomain+"/"+filename
+             logger.info(body['url'])
+             result = {"retMsg":"OK","retData":{"src":src}}
+             return response(json.dumps(result))
     except Exception as e:
-        print("Error: %s" % e)
-        return ''
-
-def fetch_https(url):
-    #if scenarioId == 'sid-003' or scenarioId == 'sid-007' or scenarioId == 'sid-0013' or scenarioId == 'sid-0016':
-     #   await gen.sleep(15)
-    
-    print('[%s] fetch_https |  url[%s]'%(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), url))    
-    header = {
-        #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36"
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) VAR/1.0.0.1"
-    }
-    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-    response = http.request('GET', url, None, header)
-    return response.data
-
-async def async_corpus(tablelist, content, html, charset):
-    res = {}
-    articles = []
-    
-    try:
-        selector = Selector(text=html,type='html')
-        items = selector.xpath(tablelist['selector']['xpath'])
-        for item in items:
-            it = {}
-            href = item.xpath('@href').get()
-            title = item.xpath('text()').get()
-            it['href'] = href
-            it['title'] = title
-            fetched = await async_fetch_https(href)
-            contentSel = Selector(text=str(fetched,encoding=charset),type='html')
-            article = contentSel.xpath(content['selector']['xpath']).xpath('text()').getall()
-            it['article'] = article
-            articles.append(it)
-        res['articles'] = articles
-        return res
-    except Exception as e:
-        print("Error: %s" % e)
-        return ''
-
-async def async_banner(container, iterator, img, title, html, sceId, uId):
-    res = {}
-    imgs = []
-    #print(container, iterator, img, title)
-    props = ApplicationContext.getContext().getInstance( p_name='var.application.configuration' )
-    datadir = props.getProperty(p_key='application.persistence.rootdir')
-    
-    selector = Selector(text=html,type='html')
-    if 'xpath' in container:
-        con = selector.xpath(container['xpath'])
-    elif 'class' in container:
-        con = selector.css(container['class'])
-    #print(con)    
-    if 'xpath' in iterator:
-        iter = con.xpath(iterator['xpath'])
-    elif 'class' in iterator:
-        iter = con.css(iterator['class'])
-    #print(iter)        
-    for item in iter:
-        it = {}
-        href = ''
-        lbl = ''
-        if 'xpath' in img:
-            href = item.xpath(img['xpath']).get()
-        elif 'class' in img:
-            href = item.css(img['class']).get()
-        elif 'tag' in img:
-            href = item.xpath(img['tag']).get()
-            
-        if 'xpath' in title:
-            lbl = item.xpath(title['xpath']).get()
-        elif 'class' in title:
-            lbl = item.css(title['class']).get()
-        elif 'tag' in img:
-            lbl = item.xpath(title['tag']).get()
-        
-        imgbytes = await async_fetch_https(href)      
-        filename =  datetime.datetime.now().strftime('%Y%m%d%H%M%S')+'_'+sceId+'_'+href[href.rfind('/')+1:]
-        contentFile = datadir+"/"+uId+'/'+ filename
-        fp = open(contentFile, 'wb+')
-        fp.write(imgbytes)
-        fp.close()
-        #href = item.xpath('.//'+img).xpath('@src').get()
-        #lbl = item.xpath('.//'+title).xpath('text()').get()
-        it['href'] = href
-        it['title'] = lbl
-        it['file'] = filename
-        #fetched = await async_fetch_https(href)
-        imgs.append(it)
-    res['imgs'] = imgs
-    return res
-    
-        
-def corpus(pageno, pages, content, parent=None):
-    page = pages[pageno]
-    selector = page['selector']
-    extract = page['extract'] if "extract" in page else 'False'
-    wholeTxt =  page['wholeTxt'] if "wholeTxt" in page else 'False'
-    
-    sel = Selector(text=content,type='html')
-    items = sel.xpath(selector['xpath'])
-    for item in items:
-        itemObj={}
-        itemObj['children']=[]
-        if extract == 'True':
-            href = item.xpath('@href').get()
-            itemObj['href'] = href
-            #body = async_fetch_https(href)
-            body = fetch_https(href)
-            corpus(pageno+1, pages, content=body, parent=itemObj)
-        if wholeTxt == 'True':
-            itemObj['text'] = item.xpath('//*/text()').getall()
-        else:
-            itemObj['text'] = item.xpath('text()').get()
-        parent['children'].append(itemObj)
-    return
-    
-def parse(content):
-    print(content)
-    selector = Selector(text=content,type='html')
-    #print('content: ', content)
-    items = selector.xpath("//ul[contains(@class, 'hot-list')]/li/a")
-    #print(items)
-    values=''
-    for item in items:
-        val={}
-        values+=item.xpath('@href').get() + ' - '
-        values+=item.xpath('text()').get() + '\n'
-        print('<a href="%s">%s</a>'%(item.xpath('@href').get(), item.xpath('text()').get()))
-    return values    
-    
-async def worker(idx, parent):
-    props = ApplicationContext.getContext().getInstance( p_name='var.application.configuration' )
-    datadir = props.getProperty(p_key='application.persistence.rootdir')
-    async for task in inq:
-                    
-        try:
-            preLen=task[0:6]
-            print('Got task, preLen: %s'%(preLen))
-            print('Task: %s'%(task[6:int(preLen)+6]))
-            taskObj = json.loads(task[6:int(preLen)+6])
-            charset = taskObj['charset']
-            print('Task charset: %s'%(charset))
-            type =  taskObj['sceType']
-            print('Task type: %s'%(type))
-            
-            body = str(task[6+int(preLen):], encoding=charset)
-            if type == 'corpus':
-                res = await async_corpus(tablelist=taskObj['pages'][0], content=taskObj['pages'][1], html=body, charset=charset)
-                articles = res['articles']
-                for article in articles:
-                    print('print result time: %s'%( datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') ))
-                    print('href: %s'%(article['href']))
-                    print('title: %s'%(article['title']))
-                    print('content: %s'%(article['article']))
-                    print('\n=======================\n')
-            if type == 'banner':
-                res = await async_banner( container=taskObj['container'], iterator=taskObj['iterator'], img=taskObj['imgTag'], title=taskObj['titleTag'], html=body, sceId=taskObj['scenarioId'], uId=taskObj['userId'] )
-                imgs = res['imgs']
-                for img in imgs:
-                    print('[%s]\nTitle: %s\nSrc: %s\nFile: %s\n==================='%(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),img['title'],img['href'],img['file']))
-#             selector = Selector(text=task[6+int(preLen):],type='html')
-#             items = selector.xpath("//ul[contains(@class, 'hot-list')]/li/a")
-#             for item in items:
-#                 print('<a href="%s">%s</a>'%(item.xpath('@href').get(), item.xpath('text()').get()))
-        except Exception as e:
-            print("Exception: %s" % (e))
-            traceback.print_exc(file=sys.stdout)
-        finally:
-            inq.task_done()
-
-    #print('worker[%s] starts'%(idx))
-#     while True:
-#         print('worker[%s] check dir - %s'%(idx, parent+'/'+idx))
-#         for file in os.listdir( parent+'/'+idx ):
-#             try:
-#                 print('work[%s] find file - %s'%(idx, file))
-#                 f = open(parent+'/'+idx+'/'+file, 'r')
-#                 props = {}
-#                 for line in f.readlines():
-#                     if len(line.strip()) > 0 and not line.startswith('#') :
-#                         pair = line.split("=")
-#                         if len(pair) == 2:
-#                             props[pair[0]] = pair[1].strip('\n\r\t ')
-#                 
-#                 html = open(props['htmlPath'], 'r')
-#                 task = json.loads(props['task'])
-#                 #values = parse(content=html.read())
-#                 values={}
-#                 values['children']=[]
-#                 values['scenarioId']=task['scenarioId']
-#                 values['userId']=task['userId']
-#                 if task['sceType'] == 'corpus':
-#                     corpus(pageno=0, pages=task['pages'], content=html.read(), parent=values)
-#                 
-#                 print(values)
-#                 res = open('F:\\var\\data\\result', 'ab+')
-#                 res.write(bytes(json.dumps(values),encoding='utf-8'))
-#                 res.close()
-#                 #f.close()
-#                 #os.remove(parent+'/'+idx+'/'+file)
-#             except Exception as e:
-#                 print("worker[%s] encounter exception: %s, parent dir: %s" % (idx, e, parent))
-#                 #remove exception file to log dir
-#                 #shutil.move(parent+'/'+idx+'/'+file, parent+'/exception/'+file)
-#             finally:
-#                 if f:
-#                     f.close()
-#                     os.remove(parent+'/'+idx+'/'+file)
-#                     
-#         await gen.sleep(5)
-
-class MainHandler(tornado.web.RequestHandler):
-    
-    async def post(self):
-        print('reqeust incoming...%s'%(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-        #print(self.request.body)
-        #print(self.request.headers)
-        #print(str(self.request.body, encoding="gb2312"))
-        await inq.put(self.request.body);
-        #await inq.put(self.request.body);
-        #print('[%s] -- put message into queue'%(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-        self.write('Gotta')
-        
-def Main(p_command=None):       
-    print('PyCloud loaded!')
-    properties = ApplicationProperties(p_command)
-    #dlist = dashboard.populate() + [ (RequestMapping.submit_fetch_url_task, MainHandler)]
-    application = tornado.web.Application([ (r'/sfut', MainHandler),])
-    application.listen(properties.getProperty(p_key='server.port'))
-    
-    vols=[]
-    partitiondir = properties.getProperty(p_key='application.persistence.fetchdir') + '/'  + properties.getProperty(p_key='application.persistence.defaultPartition') 
-    if( os.path.exists( partitiondir ) ):
-        for vol in os.listdir( partitiondir ):
-            if vol.lower().startswith('volumn'):
-                vols.append(vol)
-    print('%d volumns in partion'%(len(vols)))
-        
-    workers = gen.multi([worker( v, partitiondir ) for v in vols])
-    ioLoop = tornado.ioloop.IOLoop.current()
-    
-    #request = HttpRequest()
-    #await request.do()
-    #ApplicationContext.getContext().putInstance( p_name='var.application.inq', p_obj=inq )
-    ApplicationContext.getContext().putInstance( p_name='var.application.workers', p_obj=workers )
-    ApplicationContext.getContext().putInstance( p_name='var.application.configuration', p_obj=properties )
-    ApplicationContext.getContext().putInstance( p_name='var.application.webApplication', p_obj=application )
-    #ApplicationContext.getContext().putInstance( p_name='var.application.urlRequest', p_obj=request )
-    ApplicationContext.getContext().putInstance( p_name='var.ioloop.current', p_obj=ioLoop )
-    ioLoop.start()
-    #ioLoop.run_sync()
-  
+        logger.error(e)
+        msg = traceback.format_exc()
+        logger.error(msg)
+        return response({'retMsg':'error: %s'%(e)}, 500)
+          
 if __name__ == '__main__':
-    urllib3.contrib.pyopenssl.inject_into_urllib3()
-    props={}
-    if len(sys.argv) > 1:
-        for idx in range(1, len(sys.argv)):
-            arg = sys.argv[idx]
-            if arg.startswith("--") :
-                prop = arg[2:]
-                pair = prop.split("=")
-                props[pair[0]]=pair[1]
-                print ("command props", props)
-                
-    Main( p_command=props )                                                                          
+    logger.info("Application listening %s"%(ApplicationProperties.configure("server.port")))
+    app.run('0.0.0.0',ApplicationProperties.configure("server.port"))
